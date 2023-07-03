d82cf9c19d5a0fdc580fca53ea93d75a4c396fdc
Romulo Vieira <romulo.vieira777@gmail.com>
2023-07-03 10:33:15 -0300

    Counter OOP

---
+++ Section_07_Counter_OOP/Python_Code/01_Class_Method.py
@@ -0,0 +1,10 @@
+class Utils:
+    version = 2
+
+    @classmethod
+    def hello(self):
+        print("Hello World!")
+        print(self.version)
+
+
+Utils.hello()
---
+++ Section_07_Counter_OOP/Python_Code/02_Static_Method.py
@@ -0,0 +1,7 @@
+class MyCalss:
+    @staticmethod
+    def hello():
+        print("Hello World!")
+
+
+MyCalss.hello()
